756aedbbf4c21331a205d3842262ecb639d90083
Winifredf <Winifred.Dykes@hotmail.com>
2023-03-28 07:38:24 -0400

    created three different scenerios that my if statements will be based upon and commented them out

---
+++ If_Statements/main.py
@@ -0,0 +1,14 @@
+# I am happy
+#    Therefore I am fulfilled
+
+#I am determined
+#    I will reach my goals
+#Otherwise I will not reach my goals
+
+#Im on vacation
+#if I want to go to the beach
+#    I wear my swimsuit
+#otherwise if I am hungry
+#    I go to the resturaunt
+#otherwise if I am tired
+#    I go to sleep
