f1b4a24b08e1c60bb43ff2e4e34d282de3ff3a3c
Winifredf <Winifred.Dykes@hotmail.com>
2023-03-27 22:48:09 -0400

    passed name and age param in string into function.

--- Functions/main.py
+++ Functions/main.py
@@ -1,5 +1,5 @@
-def say_hello(name):
-    print("Hello " + name + "!")
+def say_hello(name, age):
+    print("Hello " + name + ", you are " + age + "!")
 
-say_hello("Miami")
-say_hello("Oscar")
+say_hello("Miami", "38")
+say_hello("Oscar", "44")
